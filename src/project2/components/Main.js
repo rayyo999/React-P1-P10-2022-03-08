049f776016304e433cbe985c64beb36f94a6275f
rayyo999 <i34050018@gs.ncku.edu.tw>
2022-03-09 13:40:48 +0800

    project2

--- src/project2/components/Main.js
+++ src/project2/components/Main.js
@@ -2,12 +2,14 @@ import React from "react";
 
 const Main = () => {
     return (
-        <div>
-            <h2>About</h2>
-            <p>ncku es learn  program electron force</p>
-            <h2>Interests</h2>
-            <p>soccer captain tennis communication teamwork</p>
-        </div>
+        <main className="main-p2">
+            <div>
+                <h2>About</h2>
+                <p>ncku es learn program electron force...</p>
+                <h2>Interests</h2>
+                <p>soccer captain tennis communication teamwork...</p>
+            </div>
+        </main>
     )
 }
 export default Main
